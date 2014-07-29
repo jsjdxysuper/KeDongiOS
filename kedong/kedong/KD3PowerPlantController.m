d7751a619e385aff370c1015051e7699a19687d9
Ding Xuyang <jsjdxysuper@126.com>
2014-07-29 09:35:48 +0800

    create the powerplantcontroller file

---
+++ kedong/kedong/KD3PowerPlantController.m
@@ -0,0 +1,49 @@
+//
+//  KD3PowerPlantController.m
+//  kedong
+//
+//  Created by apple on 14-7-29.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import "KD3PowerPlantController.h"
+
+@interface KD3PowerPlantController ()
+
+@end
+
+@implementation KD3PowerPlantController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
